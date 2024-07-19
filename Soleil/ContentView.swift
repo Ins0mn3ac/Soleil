898c13df46a6cc743544fc60cbb57ebdd6a5328f
Luigi Improta <luigi.improta1998@gmail.com>
2024-07-19 17:42:43 +0200

    Revert "first commit"
    This reverts commit 45dbf5bc2c42ae0be2fa05b7b3f808a1b27d6e19.

--- Soleil/ContentView.swift
+++ Soleil/ContentView.swift
@@ -7,32 +7,18 @@
 
 import SwiftUI
 
-struct MainView: View {
+struct ContentView: View {
     var body: some View {
         VStack {
-            ZStack{
-                Image("toprectangle")
-                    .position(x:180.5, y:94)
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                VStack(alignment: .leading){
-                    Group{
-                        Text("Ciao, ")
-                            .font(.custom("montserrat", size: 24))
-                        +
-                        Text("Anto!")
-                            .font(.custom("montserrat", size: 24))
-                            .bold()
-                            
-                    }.foregroundStyle(.white)
-                    Image("sol")
-                }
-            }
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
         .padding()
     }
 }
 
 #Preview {
-    MainView()
+    ContentView()
 }
--- Soleil/SoleilApp.swift
+++ Soleil/SoleilApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SoleilApp: App {
     var body: some Scene {
         WindowGroup {
-            MainView()
+            ContentView()
         }
     }
 }
