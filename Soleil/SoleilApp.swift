1ebdafb54b97ab328c5706ad6d0ee0cdb849c5d8
Luigi Improta <luigi.improta1998@gmail.com>
2024-07-19 15:30:41 +0200

    Initial Commit

---
+++ Soleil/SoleilApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoleilApp.swift
+//  Soleil
+//
+//  Created by Luigi Improta on 19/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SoleilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
